ebb32081efb8df48faba7b18afbba689556215f0
Alexandru Pintea <apaluza3@gmail.com>
2022-04-27 20:51:18 +0100

    Create chat.cpp

---
+++ chat.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Chat: ";
+
+    string v;
+
+    while (true)
+    {
+
+        getline( cin, v );
+
+        cout << "response\n";
+
+    }
+
+    return 0;
+}
